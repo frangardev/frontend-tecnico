a57acb267655c2775fd9a2d51f84c7f560e26d27
Francisco García <frangardev@outlook.com>
2023-06-08 13:22:56 -0600

    config deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/frontend-tecnico/',
+  esbuild: {
+    supported: {
+      'top-level-await': true //browsers can handle top-level-await features
+    },
+  }
 })
